3df1549bccccc7e4639a08ced8bfb69b73895754
Zachlq <58344148+Zachlq@users.noreply.github.com>
2020-06-21 09:00:37 -0400

    Add files via upload

--- DSC_550_HW_Num_3_Volume.py
+++ DSC_550_HW_Num_3_Volume.py
@@ -10,15 +10,26 @@ import math
 
 import matplotlib.pyplot as plt
 
+# Defines blank list of random values for x
 x = []
+# Defines blank list of random values for y
 y = []
 for dimension in range(50):
+# Establishes dimension variable.
     n = float(dimension)
+# Defines volume equation.
     volume = math.pi**(n/2)/math.gamma(n/2 + 1)
+# Adds n to x axis.
     x.append(n)
+# Adds volume to y axis. 
     y.append(volume)
+# Creates title. 
 plt.title('Volume vs. Dimension of a Hypersphere')
+# Labels x axis.
 plt.xlabel('Dimension (d)')
+# Labels y axis.
 plt.ylabel('Volume')
+# Plots points.
 plt.plot(x, y)
+# Displays graph.
 plt.show()
